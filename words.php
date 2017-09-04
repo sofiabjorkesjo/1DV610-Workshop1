65184ed8eedff4318252c0c7895b2360b1b15ce5
Robin Nowakowski <bobinush@gmail.com>
2017-09-04 11:00:38 +0200

    random words

--- words.php
+++ words.php
@@ -1,10 +1,13 @@
 <?php
 
 $a = "snake";
-$b = "house";
-$c = "apple";
-$d = "car";
+$b = "registration";
+$c = "website";
+$d = "computer";
 $e = "quality";
 $f = "keyboard";
 
 $arr = array($a, $b, $c, $d, $e);
+$random_keys=array_rand($arr, 1);
+
+echo "$arr[$random_keys]";
