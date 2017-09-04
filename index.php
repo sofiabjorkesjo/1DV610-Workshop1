47edf17977d294b8c43cf39ea11ce61b5bc9545f
Robin Nowakowski <bobinush@gmail.com>
2017-09-04 10:06:20 +0200

    letterbuttons

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
-<?php
 
-echo "hej";
+
+
+<html>
+<body>
+
+<form action="index.php" method="get">
+<?php
+	if( $_GET["test"] ) {
+		echo '<br />The ' . $_GET['test'] . ' submit button was pressed<br />';
+	}
+
+	$arr = range('A', 'Z');
+	echo "<input type=\"submit\" name=\"test\" value=\"";
+	echo implode("\"><input type=\"submit\" name=\"test\"value=\"",$arr);
+
+?>
+</form>
+
+</body>
+</html>
