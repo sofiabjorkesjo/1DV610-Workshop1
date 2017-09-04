6c27a1062ae6949600dbca8155f86f41f8d4f080
sofiabjorkesjo <sb223fz@student.lnu.se>
2017-09-04 14:27:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,33 +8,53 @@ session_start()
 
 <form action="index.php" method="get">
 <?php
-	var_dump($_SESSION);
+	//kollar om displayword finns, annars är displayword tom
+	$_SESSION["displayWord"] = isset($_SESSION["displayWord"]) ? $_SESSION["displayWord"] : "_____";
 
-	if (isset($_GET["test"])) {
-		if( $_GET["test"] ) {
+	$correctWord = "SNAKE";
+	$displayWord = "_____";
 
+	$guessedLetter = $_GET["letter"];
+
+	if (isset($guessedLetter)) {
+		if(in_array($guessedLetter, $_SESSION["letters"])) {
+			echo "<p>Du har redan gissat på den " . $guessedLetter . "</p>";
+		} else {
 			// Spara den gissade bokstaven i en array
-			$guessedLetters = array($_GET["test"]);
+			$guessedLetters = array($guessedLetter);
+			
 			// Hämta tidigare gissade bokstäver
 			if (isset($_SESSION["letters"])) {
 				$guessedLetters = array_merge($guessedLetters, $_SESSION["letters"]);
 			}
+
+			echo "Du har gissat på " . implode(", ", $guessedLetters);
+
 			// spara alla gissade bokstäver
 			$_SESSION["letters"] = $guessedLetters;
 
-			// echo implode("", $guessedLetters);
-			echo '<br />The ' . $_GET['test'] . ' submit button was pressed<br />';
+			echo "<p>Du gissade på " . $guessedLetter . "</p>";
+			
+			//kollar om den gissade bokstäven finns i det korrekta ordet
+			for($i = 0; $i < strlen($correctWord); $i++){
+				if($correctWord[$i] == $guessedLetter){
+					$_SESSION["displayWord"][$i] = $guessedLetter;
+				}
+			}
 		}
 	}
 
-	$arr = range('A', 'Z');
-	echo "<input type=\"submit\" name=\"test\" value=\"";
-	echo implode("\"><input type=\"submit\" name=\"test\"value=\"",$arr);
+	//när ordet är rätt vinner man
+	if ($correctWord == $_SESSION["displayWord"]) {
+		echo "<p>Du vann!</p>";
+	}
 
-    if($guessedLetters == $random_keys)
-{
-    //g
-}
+	echo "<p>" . $_SESSION["displayWord"] . "</p>";
+
+	//skriver ut alla bokstäver som knappar
+	$arr = range("A", "Z");
+	echo "<input type=\"submit\" name=\"letter\" value=\"";
+	echo implode("\"><input type=\"submit\" name=\"letter\"value=\"",$arr);
 ?>
 </form>
 
